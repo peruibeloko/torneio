7a8ab8e6703d3cf7b0770f2205029144a7cf2690
peruibeloko <cmendonca32@gmail.com>
2026-04-16 01:49:09 -0300

    update router

--- src/router.ts
+++ src/router.ts
@@ -6,7 +6,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: () => import('./pages/home.vue')
+      component: () => import('./pages/index.vue')
     },
     {
       path: '/lobby',

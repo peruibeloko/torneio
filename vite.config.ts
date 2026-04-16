33d9c492ab02daec93fcd98638f3df2c63f70481
peruibeloko <cmendonca32@gmail.com>
2026-04-16 01:42:40 -0300

    back to roots

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [deno(), vue()],
   resolve: {
     alias: {
-      '@': fromFileUrl(import.meta.resolve('./src'))
+      '@': fromFileUrl(new URL('./src', import.meta.url))
     }
   },
   server: {
